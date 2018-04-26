efd6975289659ca59673b433f1442a07df6d6619
arttanzl <630804990@qq.com>
2018-04-26 23:55:14 +0800

    完成首页基本功能

--- src/js/index.js
+++ src/js/index.js
@@ -14,5 +14,12 @@ $(function(){
 
 	// 全局搜索
 	$('.m-header .ui-select').custoomSelect();
+
+	// 首页左右切换
+	$('#products .hd').on('click',"span",function(){
+		var me = $(this), index = me.index();
+		me.addClass('on').siblings().removeClass();
+		$('#products').find('.rel').eq(index+1).show().siblings().hide();
+	});
 	
 });
