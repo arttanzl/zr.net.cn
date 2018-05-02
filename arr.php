97383eee46a5e5ccf76d60261335cb95233f57ad
arttanzl <630804990@qq.com>
2018-05-03 00:43:58 +0800

    电容电阻分类

---
+++ arr.php
@@ -0,0 +1,448 @@
+<?php
+
+header('Content-Type:text/html;charset=utf-8');
+$arr = Array(
+    '0' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 1 ,
+            'name' => '电容' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 3 ,
+                            'name' => 'MLCC'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 12 ,
+                            'name' => '钽电容'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 14 ,
+                            'name' => '薄膜电容'
+                        ),
+
+                    '3' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 16 ,
+                            'name' => '铝电解电容'
+                        ),
+
+                    '4' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 18 ,
+                            'name' => '超级电容'
+                        ),
+
+                    '5' => Array
+                        (
+                            'fid' => 1 ,
+                            'cate_id' => 34 ,
+                            'name' => '插件陶瓷电容'
+                        )
+
+                )
+
+        ),
+
+    '1' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 2 ,
+            'name' => '电阻' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 2 ,
+                            'cate_id' => 4 ,
+                            'name' => '贴片电阻'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 2 ,
+                            'cate_id' => 35 ,
+                            'name' => '插件电阻'
+                        )
+
+                )
+
+        ),
+
+    '2' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 5 ,
+            'name' => '电感' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 5 ,
+                            'cate_id' => 56 ,
+                            'name' => '贴片电感'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 5 ,
+                            'cate_id' => 57 ,
+                            'name' => '插件电感'
+                        )
+
+                )
+
+        ),
+
+    '3' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 19 ,
+            'name' => '磁珠' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 19 ,
+                            'cate_id' => 20 ,
+                            'name' => '贴片磁珠'
+                        )
+
+                )
+
+        ),
+
+    '4' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 21 ,
+            'name' => '二极管' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 22 ,
+                            'name' => '齐纳二极管'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 23 ,
+                            'name' => '开关二极管'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 24 ,
+                            'name' => '肖特基二极管'
+                        ),
+
+                    '3' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 25 ,
+                            'name' => '普通整流二极管'
+                        ),
+
+                    '4' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 26 ,
+                            'name' => '肖特基整流二极管'
+                        ),
+
+                    '5' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 27 ,
+                            'name' => '快恢复二极管'
+                        ),
+
+                    '6' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 28 ,
+                            'name' => 'TVS管'
+                        ),
+
+                    '7' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 29 ,
+                            'name' => 'PIN二极管'
+                        ),
+
+                    '8' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 36 ,
+                            'name' => 'ESD管'
+                        ),
+
+                    '9' => Array
+                        (
+                            'fid' => 21 ,
+                            'cate_id' => 37 ,
+                            'name' => '高压二极管'
+                        )
+
+                )
+
+        ),
+
+    '5' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 30 ,
+            'name' => '三极管' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 30 ,
+                            'cate_id' => 31 ,
+                            'name' => '通用三极管'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 30 ,
+                            'cate_id' => 32 ,
+                            'name' => '数字三极管'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 30 ,
+                            'cate_id' => 33 ,
+                            'name' => '达林顿管'
+                        )
+
+                )
+
+        ),
+
+    '6' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 39 ,
+            'name' => 'MOS管' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 39 ,
+                            'cate_id' => 40 ,
+                            'name' => '低压MOS管'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 39 ,
+                            'cate_id' => 42 ,
+                            'name' => '中高压MOS管'
+                        )
+
+                )
+
+        ),
+
+    '7' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 59 ,
+            'name' => '整流桥' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 59 ,
+                            'cate_id' => 60 ,
+                            'name' => '通用整流桥'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 59 ,
+                            'cate_id' => 61 ,
+                            'name' => '快恢复整流桥'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 59 ,
+                            'cate_id' => 62 ,
+                            'name' => '高效恢复整流桥'
+                        ),
+
+                    '3' => Array
+                        (
+                            'fid' => 59 ,
+                            'cate_id' => 64 ,
+                            'name' => '超快恢复整流桥'
+                        ),
+
+                    '4' => Array
+                        (
+                            'fid' => 59 ,
+                            'cate_id' => 65 ,
+                            'name' => '肖特基整流桥'
+                        )
+
+                )
+
+        ),
+
+    '8' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 66 ,
+            'name' => '晶振' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 67 ,
+                            'name' => '普通晶体振荡器'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 69 ,
+                            'name' => '温补晶体振荡器'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 70 ,
+                            'name' => '压控温补晶体振荡器'
+                        ),
+
+                    '3' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 71 ,
+                            'name' => '恒温晶体振荡器'
+                        ),
+
+                    '4' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 72 ,
+                            'name' => '普通晶体'
+                        ),
+
+                    '5' => Array
+                        (
+                            'fid' => 66 ,
+                            'cate_id' => 73 ,
+                            'name' => '热敏晶体'
+                        )
+
+                )
+
+        ),
+
+    '9' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 74 ,
+            'name' => '热敏电阻' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 74 ,
+                            'cate_id' => 76 ,
+                            'name' => '热敏电阻'
+                        )
+
+                )
+
+        ),
+
+    '10' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 75 ,
+            'name' => '压敏电阻' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 75 ,
+                            'cate_id' => 77 ,
+                            'name' => '压敏电阻'
+                        )
+
+                )
+
+        ),
+
+    '11' => Array
+        (
+            'fid' => 0 ,
+            'cate_id' => 78 ,
+            'name' => '滤波器' ,
+            'list' => Array
+                (
+                    '0' => Array
+                        (
+                            'fid' => 78 ,
+                            'cate_id' => 79 ,
+                            'name' => '共模滤波器'
+                        ),
+
+                    '1' => Array
+                        (
+                            'fid' => 78 ,
+                            'cate_id' => 80 ,
+                            'name' => '片式三端子滤波器'
+                        ),
+
+                    '2' => Array
+                        (
+                            'fid' => 78 ,
+                            'cate_id' => 81 ,
+                            'name' => '引线型滤波器'
+                        ),
+
+                    '3' => Array
+                        (
+                            'fid' => 78 ,
+                            'cate_id' => 82 ,
+                            'name' => '电源线用滤波器'
+                        ),
+
+                    '4' => Array
+                        (
+                            'fid' => 78 ,
+                            'cate_id' => 83 ,
+                            'name' => '音频线用噪音滤波器'
+                        )
+
+                )
+
+        )
+);
+
+print_r($arr)
+
+?>
